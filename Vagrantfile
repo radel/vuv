825b1a124a2733865b69298cdf1ed98064877350
Marco Bonomo <marco.bonomo@gmail.com>
2016-04-06 22:03:23 +0200

    install vesta - first commit

--- Vagrantfile
+++ Vagrantfile
@@ -1,17 +1,17 @@
 Vagrant.configure("2") do |config|
-	
+
 	# Specify the base box
-	config.vm.box = "primalskill/ubuntu-trusty64"
-	
+	config.vm.box = "ubuntu/trusty64"
+
 	# Setup port forwarding
 	config.vm.network :forwarded_port, guest: 80, host: 8931, auto_correct: true
 
     # Setup synced folder
-    config.vm.synced_folder "./", "/var/www", create: true, group: "www-data", owner: "www-data"
+    config.vm.synced_folder "./code", "/home/vagrant/code", create: true, group: "vagrant", owner: "vagrant"
 
     # VM specific configs
     config.vm.provider "virtualbox" do |v|
-    	v.name = "SitePoint Test Vagrant"
+    	v.name = "VUV test vagrant"
     	v.customize ["modifyvm", :id, "--memory", "1024"]
     end
 
--- provision/setup.sh
+++ provision/setup.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
 
 echo "Provisioning virtual machine..."
-
 # Git
 echo "Installing Git"
 apt-get install git -y > /dev/null
+# Node
+echo "Installing Node"
+sudo apt-get install nodejs -y > /dev/null
+sudo apt-get install npm -y > /dev/null
 
-# Nginx
-echo "Installing Nginx"
-apt-get install nginx -y > /dev/null
+echo "Installing Vesta"
 
-# PHP
-echo "Updating PHP repository"
-apt-get install python-software-properties build-essential -y > /dev/null
-add-apt-repository ppa:ondrej/php5 -y > /dev/null
-apt-get update > /dev/null
-
-echo "Installing PHP"
-apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
-
-echo "Installing PHP extensions"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
-
-# MySQL 
-echo "Preparing MySQL"
-apt-get install debconf-utils -y > /dev/null
-debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
-
-echo "Installing MySQL"
-apt-get install mysql-server -y > /dev/null
-
-# Nginx Configuration
-echo "Configuring Nginx"
-cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
-ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
-
-rm -rf /etc/nginx/sites-available/default
-
-# Restart Nginx for the config to take effect
-service nginx restart > /dev/null
-
+echo "We will install vesta as root.":
+echo "Switch to root user...":
+sudo su;
+cd ~;
+curl -O http://vestacp.com/pub/vst-install.sh;
+bash vst-install.sh --force -e marco.bonomo2@gmail.com;
+rm vst-install.sh;
+echo "Removed VestaCP installer.";
+echo "Finished installation of Vesta Control Panel.";
+echo "Exiting root user..."
+exit;
+echo "Please take note of your admin password above. Please enter it below, to change your password.";
+su admin;
+echo "Enter your password below to change your password.";
+passwd;
+exit;
 echo "Finished provisioning."
